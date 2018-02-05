03faa6f5abd6b9d72423f46e208e033872065b34
Johan <jfmirag@gmail.com>
2018-02-05 11:51:50 -0500

    se usa paquete plotly para algunos graficos y se ocultaln mensajes y warning

--- codigospagina.R
+++ codigospagina.R
@@ -11,3 +11,32 @@ qplot(temperature, pressure, data=d, geom=c("point", "smooth"), method="lm", for
 ```{r, results='asis'}
 knitr::kable(summary(regresion2))
 ```
+
+library(plotly)
+p <- plot_ly(alpha = 0.1) %>%
+  add_histogram(x = cars$speed) %>%
+  add_histogram(x = cars$dist) %>%
+  layout(barmode = "overlay")
+p
+
+
+p <- plot_ly(data = iris, x = ~Sepal.Length, y = ~Petal.Length, color = ~Species)
+
+
+p <- plot_ly(x = cars$speed, type = "histogram")
+
+
+p1 <- plot_ly(x = cars$speed, type = "histogram")
+
+p2 <- plot_ly(x = cars$speed, type = "histogram")
+
+subplot(p1, p2) %>% hide_legend()
+regresion <- lm(cars$dist ~ cars$speed, data = cars)
+abline(regresion,col="red")
+p <- plot_ly(data = cars,x=cars$speed,y=cars$dist,type = "scatter")
+p
+#add_trace(y = ~regresion, name = 'Regresion')
+add_lines(y = ~regresion)
+
+
+
