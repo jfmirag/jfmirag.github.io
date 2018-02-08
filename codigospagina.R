34d83752f32028061f77484c2dde593ca09e3ea2
Johan <jfmirag@gmail.com>
2018-02-07 22:26:10 -0500

    se cambia el contenido

--- codigospagina.R
+++ codigospagina.R
@@ -39,4 +39,24 @@ p
 add_lines(y = ~regresion)
 
 
+##################lo de antes
 
+library(plotly)
+plot(cars,main="Velocidad vs Distancia",xlab="Velocidad",ylab="Distancia",col="blue")
+regresion <- lm(cars$dist ~ cars$speed, data = cars)
+abline(regresion,col="red")
+#par(mfrow=c(1,2))
+#hist(cars$speed,xlab = "Velocidad",ylab = "Frecuencia",main = "Histigrama de velocidad",col = "green")
+#hist(cars$dist,xlab = "Distancia",ylab = "Frecuencia",main = "Histigrama de distancia",col = "yellow")
+plot_ly(data = cars,x=cars$speed,y=cars$dist,type = "scatter")
+p1 <- plot_ly(x = cars$speed, type = "histogram")
+p2 <- plot_ly(x = cars$speed, type = "histogram")
+subplot(p1, p2)
+
+
+d <- subset (pressure, temperature!=0)
+regresion2 <- lm(pressure ~ temperature + I(temperature^2), data = d)
+#summary(regresion2)
+plot(pressure,xlab="temperatura",ylab="presion",main="Temperatura vs Presion",col="red")
+library(ggplot2)
+qplot(temperature, pressure, data=d, geom=c("point", "smooth"), method="lm", formula= y ~ poly(x, 2),main ="Ajuste modelo cuadratico",xlab = "Temperatura",ylab = "Presion")
